74bf9f665322cbe60b9cc3f6655d31ccce0375c8
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-03-18 22:49:06 +0200

    Remove unnecessary test

---
+++ app/Services/OrderService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use App\Jobs\IncrementOrderQuantity;
+use App\Models\Product;
+
+class OrderService
+{
+    public function createOrder($order, $items)
+    {
+        $order_total = 0;
+        foreach ($items as $item){
+            $order->items()->create($item);
+
+            $product = Product::find($item['product_id']);
+            $order_total += $product->price;
+            IncrementOrderQuantity::dispatch($product, $item['quantity']);
+        }
+
+        return $order_total;
+    }
+}
