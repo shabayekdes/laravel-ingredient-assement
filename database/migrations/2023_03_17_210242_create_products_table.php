48ce41161c4178c63e9eb8960861f8319394957a
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-03-18 22:04:01 +0200

    Fix migrations add price and order_id columns

--- database/migrations/2023_03_17_210242_create_products_table.php
+++ database/migrations/2023_03_17_210242_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->decimal('price');
             $table->string('image')->default('images/products/default.png');
             $table->timestamps();
         });
--- database/migrations/2023_03_18_032405_create_orders_table.php
+++ database/migrations/2023_03_18_032405_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->decimal('order_total');
+            $table->decimal('order_total')->nullable();
             $table->timestamps();
         });
     }
